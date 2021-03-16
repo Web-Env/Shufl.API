3993de6c313a6703ce3dee9b9b1d5cb339cd58a3
AdamBOD <adambarryodonovan@gmail.com>
2021-03-16 21:39:16 +0000

    Added Unit Test

---
+++ Tests/Shufl.API.Models.Tests/SearchHelperTests.cs
@@ -0,0 +1,23 @@
+﻿using Shufl.API.Models.Helpers;
+using Xunit;
+
+namespace Shufl.API.Models.Tests
+{
+    [Trait("Category", "Unit")]
+    public class SearchHelperTests
+    {
+        [Fact]
+        public void RandInt_ShouldReturnIntInRange()
+        {
+            //Arrange
+            var min = 0;
+            var max = 100;
+
+            //Act
+            var randInt = SearchHelper.RandInt(min, max);
+
+            //Assert
+            Assert.InRange(randInt, min, max);
+        }
+    }
+}
