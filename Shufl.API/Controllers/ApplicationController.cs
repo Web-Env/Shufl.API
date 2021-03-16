6ce7840e2b9c92c99ef1801a6f97b4be414b2c95
AdamBOD <adambarryodonovan@gmail.com>
2021-03-16 22:13:13 +0000

    Add ApplicationController and Unit Tests

---
+++ Shufl.API/Controllers/ApplicationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shufl.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApplicationController : Controller
+    {
+        [HttpGet("Version")]
+        public ActionResult<string> GetVersion()
+        {
+            var version = typeof(Startup).Assembly.GetName().Version.ToString();
+            return version;
+        }
+    }
+}
