5acd723b2be756ab14f193af5cdda6a9c3b07bf2
AdamBOD <adambarryodonovan@gmail.com>
2021-04-02 00:38:33 +0100

    Setup album search (#9)

---
+++ Shufl.API.Infrastructure/Helpers/ReleaseDateParsingHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shufl.API.Infrastructure.Helpers
+{
+    public static class ReleaseDateParsingHelper
+    {
+        public static DateTime ParseReleaseDateToDateTime(string releaseDate, string releaseDatePrecision)
+        {
+            if (releaseDatePrecision == "year")
+            {
+                return DateTime.ParseExact(releaseDate, "yyyy", CultureInfo.InvariantCulture);
+            }
+            else if (releaseDatePrecision == "month")
+            {
+                return DateTime.ParseExact(releaseDate, "yyyy-MM", CultureInfo.InvariantCulture);
+            }
+            else if (releaseDatePrecision == "day")
+            {
+                return DateTime.Parse(releaseDate);
+            }
+            else
+            {
+                return DateTime.Parse(releaseDate);
+            }
+        }
+    }
+}
