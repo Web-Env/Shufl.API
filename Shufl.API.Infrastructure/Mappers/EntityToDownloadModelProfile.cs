e05d5b6db5e7aba7eab506b3be16f190b0306719
AdamBOD <adambarryodonovan@gmail.com>
2021-04-21 20:44:07 +0100

    Updated GroupDownloadModel (#23)

--- Shufl.API.Infrastructure/Mappers/EntityToDownloadModelProfile.cs
+++ Shufl.API.Infrastructure/Mappers/EntityToDownloadModelProfile.cs
@@ -43,11 +43,8 @@ namespace Shufl.API.Infrastructure.Mappers
             CreateMap<UserImage, ImageDownloadModel>();
 
             CreateMap<Group, GroupDownloadModel>()
-                .ForMember(dest => dest.Members, src => src.MapFrom(g => g.GroupMembers))
+                .ForMember(dest => dest.Members, src => src.MapFrom(g => g.GroupMembers.Select(gm => gm.User)))
                 .ForMember(dest => dest.CreatedBy, src => src.MapFrom(g => g.CreatedByNavigation));
-
-            CreateMap<GroupMember, GroupMemberDownloadModel>()
-                .ForMember(dest => dest.User, src => src.MapFrom(gm => gm.User));
 
             CreateMap<GroupSuggestion, GroupSuggestionDownloadModel>()
                 .ForMember(dest => dest.CreatedBy, src => src.MapFrom(g => g.CreatedByNavigation));
