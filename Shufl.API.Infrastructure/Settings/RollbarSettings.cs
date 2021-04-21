03f718946772f78d27bab2a5f822f29e161a57e6
AdamBOD <adambarryodonovan@gmail.com>
2021-04-21 23:51:36 +0100

    Setup Rollbar (#27)

---
+++ Shufl.API.Infrastructure/Settings/RollbarSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Shufl.API.Infrastructure.Settings
+{
+    public class RollbarSettings
+    {
+        public string AccessToken { get; set; }
+
+        public string Environment { get; set; }
+    }
+}
