2424ba82a9bde31a9112ba77b10dc1533ea80d8d
AdamBOD <adambarryodonovan@gmail.com>
2021-09-06 18:52:33 +0100

    Setup RelatedGroupAlbumId as part of GroupAlbum (#43)

--- Shufl.API.UploadModels/Group/GroupAlbumUploadModel.cs
+++ Shufl.API.UploadModels/Group/GroupAlbumUploadModel.cs
@@ -1,10 +1,14 @@
-﻿namespace Shufl.API.UploadModels.Group
+﻿using System;
+
+namespace Shufl.API.UploadModels.Group
 {
     public class GroupAlbumUploadModel
     {
         public string GroupIdentifier { get; set; }
 
         public string AlbumIdentifier { get; set; }
+
+        public Guid? RelatedGroupAlbumId { get; set; }
 
         public bool IsRandom { get; set; }
     }
